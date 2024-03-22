91ae729ed95de955289c1b94d1533a85faec3e1c
RustyBeans <107262647+RustyBeans@users.noreply.github.com>
2024-03-22 05:30:52 -0700

    first commit

---
+++ RadioBeans/frmMain.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace RadioBeans
+{
+	partial class frmMain
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			components = new System.ComponentModel.Container();
+			btnPlay = new Button();
+			btnStop = new Button();
+			cmbSongList = new ComboBox();
+			btnChooseFolder = new Button();
+			btnClearList = new Button();
+			label1 = new Label();
+			tmr1Second = new System.Windows.Forms.Timer(components);
+			pbxCover = new PictureBox();
+			label2 = new Label();
+			lblCurrentlyPlaying = new Label();
+			tbrVolume = new TrackBar();
+			tbrSeek = new TrackBar();
+			((System.ComponentModel.ISupportInitialize)pbxCover).BeginInit();
+			((System.ComponentModel.ISupportInitialize)tbrVolume).BeginInit();
+			((System.ComponentModel.ISupportInitialize)tbrSeek).BeginInit();
+			SuspendLayout();
+			// 
+			// btnPlay
+			// 
+			btnPlay.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+			btnPlay.Location = new Point(12, 594);
+			btnPlay.Name = "btnPlay";
+			btnPlay.Size = new Size(75, 23);
+			btnPlay.TabIndex = 0;
+			btnPlay.Text = "Play";
+			btnPlay.UseVisualStyleBackColor = true;
+			btnPlay.Click += btnPlay_Click;
+			// 
+			// btnStop
+			// 
+			btnStop.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+			btnStop.Location = new Point(613, 594);
+			btnStop.Name = "btnStop";
+			btnStop.Size = new Size(75, 23);
+			btnStop.TabIndex = 1;
+			btnStop.Text = "Stop";
+			btnStop.UseVisualStyleBackColor = true;
+			btnStop.Click += btnStop_Click;
+			// 
+			// cmbSongList
+			// 
+			cmbSongList.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			cmbSongList.FormattingEnabled = true;
+			cmbSongList.Location = new Point(129, 624);
+			cmbSongList.Name = "cmbSongList";
+			cmbSongList.Size = new Size(442, 23);
+			cmbSongList.TabIndex = 2;
+			cmbSongList.SelectedIndexChanged += cmbSongList_SelectedIndexChanged;
+			// 
+			// btnChooseFolder
+			// 
+			btnChooseFolder.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+			btnChooseFolder.Location = new Point(12, 623);
+			btnChooseFolder.Name = "btnChooseFolder";
+			btnChooseFolder.Size = new Size(111, 23);
+			btnChooseFolder.TabIndex = 3;
+			btnChooseFolder.Text = "Choose folder...";
+			btnChooseFolder.UseVisualStyleBackColor = true;
+			btnChooseFolder.Click += btnChooseFolder_Click;
+			// 
+			// btnClearList
+			// 
+			btnClearList.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+			btnClearList.Location = new Point(577, 623);
+			btnClearList.Name = "btnClearList";
+			btnClearList.Size = new Size(111, 23);
+			btnClearList.TabIndex = 4;
+			btnClearList.Text = "Clear list...";
+			btnClearList.UseVisualStyleBackColor = true;
+			btnClearList.Click += btnClearList_Click;
+			// 
+			// label1
+			// 
+			label1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+			label1.AutoSize = true;
+			label1.Location = new Point(12, 492);
+			label1.Name = "label1";
+			label1.Size = new Size(91, 15);
+			label1.TabIndex = 5;
+			label1.Text = "DEV bit position";
+			label1.Click += label1_Click;
+			// 
+			// tmr1Second
+			// 
+			tmr1Second.Tick += tmr1Second_Tick;
+			// 
+			// pbxCover
+			// 
+			pbxCover.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			pbxCover.Location = new Point(12, 12);
+			pbxCover.Name = "pbxCover";
+			pbxCover.Size = new Size(676, 474);
+			pbxCover.TabIndex = 8;
+			pbxCover.TabStop = false;
+			// 
+			// label2
+			// 
+			label2.AutoSize = true;
+			label2.Location = new Point(495, 421);
+			label2.Name = "label2";
+			label2.Size = new Size(38, 15);
+			label2.TabIndex = 9;
+			label2.Text = "label2";
+			// 
+			// lblCurrentlyPlaying
+			// 
+			lblCurrentlyPlaying.AutoSize = true;
+			lblCurrentlyPlaying.Location = new Point(93, 380);
+			lblCurrentlyPlaying.Name = "lblCurrentlyPlaying";
+			lblCurrentlyPlaying.Size = new Size(177, 15);
+			lblCurrentlyPlaying.TabIndex = 10;
+			lblCurrentlyPlaying.Text = "The Currently Playing Song.mp3";
+			// 
+			// tbrVolume
+			// 
+			tbrVolume.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			tbrVolume.LargeChange = 0;
+			tbrVolume.Location = new Point(12, 543);
+			tbrVolume.Name = "tbrVolume";
+			tbrVolume.Size = new Size(676, 45);
+			tbrVolume.TabIndex = 11;
+			tbrVolume.TickStyle = TickStyle.None;
+			tbrVolume.Scroll += tbrVolume_Scroll;
+			// 
+			// tbrSeek
+			// 
+			tbrSeek.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			tbrSeek.LargeChange = 0;
+			tbrSeek.Location = new Point(12, 492);
+			tbrSeek.Name = "tbrSeek";
+			tbrSeek.Size = new Size(676, 45);
+			tbrSeek.TabIndex = 12;
+			tbrSeek.Scroll += tbrSeek_Scroll;
+			// 
+			// frmMain
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			BackColor = SystemColors.Control;
+			ClientSize = new Size(700, 658);
+			Controls.Add(tbrSeek);
+			Controls.Add(tbrVolume);
+			Controls.Add(lblCurrentlyPlaying);
+			Controls.Add(label2);
+			Controls.Add(pbxCover);
+			Controls.Add(label1);
+			Controls.Add(btnClearList);
+			Controls.Add(btnChooseFolder);
+			Controls.Add(cmbSongList);
+			Controls.Add(btnStop);
+			Controls.Add(btnPlay);
+			Name = "frmMain";
+			Text = "Radio Beans";
+			((System.ComponentModel.ISupportInitialize)pbxCover).EndInit();
+			((System.ComponentModel.ISupportInitialize)tbrVolume).EndInit();
+			((System.ComponentModel.ISupportInitialize)tbrSeek).EndInit();
+			ResumeLayout(false);
+			PerformLayout();
+		}
+
+		#endregion
+
+		private Button btnPlay;
+		private Button btnStop;
+		private ComboBox cmbSongList;
+		private Button btnChooseFolder;
+		private Button btnClearList;
+		private Label label1;
+		private System.Windows.Forms.Timer tmr1Second;
+		private PictureBox pbxCover;
+		private Label label2;
+		private Label lblCurrentlyPlaying;
+		private TrackBar tbrVolume;
+		private TrackBar tbrSeek;
+	}
+}
---
+++ RadioBeans/frmMain.cs
@@ -0,0 +1,200 @@
+using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.Devices;
+using System.Media;
+using System.Windows.Forms;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+using System.Windows.Forms.VisualStyles;
+
+namespace RadioBeans
+{
+
+	public partial class frmMain : Form
+	{
+		private WaveOutEvent outputDevice;
+		private AudioFileReader audioFile;
+		private OpenFileDialog audioLibrary = new OpenFileDialog();
+		private FolderBrowserDialog fbd = new FolderBrowserDialog();
+		private string[] clipLibrary;
+
+		
+		private bool isPlaying;
+
+		// true if cover, false if songlist
+		// private bool currentImage;
+		private Image coverImage;
+		private Image tracklistImage;
+		private bool foundCover = false;
+		private bool foundTracklist = false;
+
+		public frmMain()
+		{
+			InitializeComponent();
+			FormLoad();
+		}
+		public void FormLoad()
+		{
+
+			tbrVolume.Maximum = 100;
+			tbrVolume.Value = 10;
+			pbxCover.SizeMode = PictureBoxSizeMode.Zoom;
+			tmr1Second.Interval = 1000; // 1 sec
+
+			if (outputDevice == null)
+			{
+				outputDevice = new WaveOutEvent();
+			}
+			//outputDevice.Volume = tbrVolume.Value;
+		}
+
+		private void btnClearList_Click(object sender, EventArgs e)
+		{
+			cmbSongList.Items.Clear();
+		}
+		private void btnPlay_Click(object sender, EventArgs e)
+		{
+			
+			StartPlaying();
+			isPlaying = true;
+			label2.Text = "true";
+		}
+		private void btnStop_Click(object sender, EventArgs e)
+		{
+			StopPlaying();
+			isPlaying = false;
+			label2.Text = "false";
+		}
+
+		private void btnChooseFolder_Click(object sender, EventArgs e)
+		{
+			DialogResult result = fbd.ShowDialog();
+			if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+			{
+				clipLibrary = Directory.GetFiles(fbd.SelectedPath);
+				foreach (string clip in clipLibrary)
+				{
+					if (clip.EndsWith(".mp3"))
+					{
+						cmbSongList.Items.Add(clip);
+					}
+					if (clip.EndsWith("Cover.png") || clip.EndsWith("cover.png"))
+					{
+						coverImage = Image.FromFile(clip);
+						foundCover = true;
+						//currentImage = true;
+						pbxCover.Image = coverImage;
+					}
+					if (clip.EndsWith("Tracklist.png"))
+					{
+						tracklistImage = Image.FromFile(clip);
+						foundTracklist = true;
+					}
+
+					// part of swapping current image
+					/*else
+					{
+						foundCover = false;
+						currentImage = false;
+					}*/
+				}
+
+				System.Windows.Forms.MessageBox.Show("Files found: " + clipLibrary.Length.ToString() + '\n' + "Cover found: " + foundCover.ToString());
+			}
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+		}
+
+		private void tmr1Second_Tick(object sender, EventArgs e)
+		{
+			
+		}
+		private void StartPlaying()
+		{
+			if (outputDevice == null)
+			{
+				outputDevice = new WaveOutEvent();
+			}
+			if (audioFile == null)
+			{
+				audioFile = new AudioFileReader(cmbSongList.SelectedItem.ToString());
+				outputDevice.Init(audioFile);
+				tbrSeek.Maximum = System.Convert.ToInt32(audioFile.Length);
+			}
+			outputDevice.Play();
+			tmr1Second.Start();
+
+
+		}
+		private void StopPlaying()
+		{
+			if (outputDevice != null)
+			{
+				tmr1Second.Stop();
+				outputDevice?.Stop();
+				outputDevice.Dispose();
+				outputDevice = null;
+				audioFile.Dispose();
+				audioFile = null;
+			}
+			
+		}
+
+		// trying to click the image to swap between songlist and cover image for fun
+		/*private void pbxCover_Click(object sender, EventArgs e)
+		{
+			foreach (string clip in clipLibrary)
+			{
+				if (clip.EndsWith("songlist.png") && currentImage == true)
+				{
+					pbxCover.Image = Image.FromFile(clip);
+					currentImage = false;
+				}
+				if (clip.EndsWith("cover.png") && currentImage == true)
+				{
+					pbxCover.Image = Image.FromFile(clip);
+					currentImage = true;
+				}
+			}
+		}*/
+		private void waveViewer1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void volVolume_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void cmbSongList_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			if (isPlaying == true)
+			{
+				StopPlaying();
+				StartPlaying();
+			}
+			
+		}
+
+		private void tbrVolume_Scroll(object sender, EventArgs e)
+		{
+			if (outputDevice != null)
+			{
+				outputDevice.Volume = tbrVolume.Value * .01f;
+			}
+			
+		}
+
+		private void tbrSeek_Scroll(object sender, EventArgs e)
+		{
+		 if (audioFile != null)
+			{
+				audioFile.Position = tbrSeek.Value;
+			}
+		}
+
+		
+	}
+}
